c32e4e0fdfd9c4fb4a5b99e8bd1e2205fc2dcb5f
Wittaya <indochinahub@gmail.com>
2020-02-05 17:20:02 +0700

    commit

---
+++ ConvertChineseToPinyin/program.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+#
+# You need to fill the blank line at the end of the source file.
+#
+
+import re
+import pinyin
+
+source_file = open('source.txt', encoding='utf-8', mode='r')
+result_file = open('result.txt', encoding='utf-8', mode='w')
+
+OneLineText = ""
+for line in source_file :
+    line = line.strip()
+    line = pinyin.get(line)
+    result_file.write(line + '\n' )     
+
+
