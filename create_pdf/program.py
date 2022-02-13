f8e9b34c344f0f7e4078eb1978d25f7bb51b92a3
wittaya <indochinahub@gmail.com>
2022-02-13 07:44:30 +0700

    ปรับปรุง

--- create_pdf/program.py
+++ create_pdf/program.py
@@ -25,28 +25,9 @@ for i in range(1, 2):
     pdf.set_xy( 65, init_y_for_image )
     text = '1. x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x\n'
     text = text + '2. x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x'
-    pdf.multi_cell( 0, 10, text, border = 1,  fill = False, ln = 1)
+    pdf.multi_cell( 0, 10, text, border = 1,  fill = False)
     
-    pdf.set_xy( 65, init_y_for_image + image_offset )
-    text = '1. x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x\n'
-    text = text + '2. x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x'
-    pdf.multi_cell( 0, 10, text, border = 1,  fill = False, ln = 1)
 
-
-    pdf.set_xy( 65, init_y_for_image + (image_offset * 2) )
-    text = '1. x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x\n'
-    text = text + '2. x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x'
-    pdf.multi_cell( 0, 10, text, border = 1,  fill = False, ln = 1)
-    
-    pdf.set_xy( 65, init_y_for_image + (image_offset * 3) )
-    text = '1. x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x\n'
-    text = text + '2. x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x'
-    pdf.multi_cell( 0, 10, text, border = 1,  fill = False, ln = 1)
-
-    pdf.set_xy( 65, init_y_for_image + (image_offset * 4) )
-    text = '1. x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x\n'
-    text = text + '2. x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x'
-    pdf.multi_cell( 0, 10, text, border = 1,  fill = False, ln = 1)
 
 pdf.output('001.pdf')
 
