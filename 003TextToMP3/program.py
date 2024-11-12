1baa450b9223be82a0ec469b738388cb03a045e2
Wittaya Wijit <indochinahub@gmail.com>
2024-11-12 13:09:14 +0700

    commit

---
+++ 003TextToMP3/program.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+#
+# You need to fill the blank line at the end of the source file.
+#
+from gtts import gTTS
+import os
+import time
+
+source_file = open('source.txt', encoding='utf-8', mode='r')
+result_file = open('result.txt', encoding='utf-8', mode='w')
+
+for line in source_file :
+    line = line.strip()
+    splitLine = line.split("\t")
+    
+    text_id = splitLine[0]
+    text = splitLine[1]
+    
+    text = text.replace("[newline]"," ")
+    text = text.replace(" It "," it ")
+    # Lang = vi, ja, en, km, zh-cn, ko, th
+    tts = gTTS(text , lang='en')
+    tts.save(""+splitLine[0].strip()+".mp3" )
+	
+    print( text_id + "\t" + text )
+    
+    time.sleep(2)
+
