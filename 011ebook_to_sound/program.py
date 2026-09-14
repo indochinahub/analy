3997868b33f708aed72ee1adff66c132698ca3fb
Wittaya Wijit <indochinahub@gmail.com>
2026-09-14 15:00:50 +0700

    commit

--- 011ebook_to_sound/program.py
+++ 011ebook_to_sound/program.py
@@ -1,4 +1,44 @@
 # main.py
 import mymodule
 
-print(mymodule.greeting("Jonathan"))
+ 
+with open("source.txt", encoding='utf-8', mode='r') as file:
+    text = file.read()
+
+result_file = open('result.txt', encoding='utf-8', mode='w')    
+
+arr_group_of_line = mymodule.get_group_of_line(text)
+
+arr_group = []
+for group in arr_group_of_line:
+    arr_sentence = group.split('\n')
+    if len(arr_sentence) < 2:
+        print ("Error: len()< 2 : " + arr_sentence[0] + '\n*********')
+    else:
+        #print(arr_sentence)
+        arr_group.append(arr_sentence)
+
+output_text = ""
+output_text2 = ""
+for group in arr_group:
+    topic = group[0]
+
+    group_text = ""
+    for i in range(len(group)):
+        if i > 0:
+            group_text = group_text + " " + str(i) + ") " + group[i]
+            output_text2 = output_text2 + topic +  "\t" + group[i] + "\n"
+    
+    output_text = output_text + topic + "\t" + group_text + "\n"
+
+print(output_text)
+result_file.write (output_text.strip()  +"\n******\n" + output_text2.strip())
+#result_file.write (output_text2.strip())
+result_file.close ()
+
+
+
+
+    
+
+
